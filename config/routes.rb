65b471e51b43806a44bbd37128dfdbc3b36f4330
= <=>
2018-02-11 06:11:52 +0000

    added friendships relationship

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stocks' => 'stocks#search'
   resources :user_stocks, only: [:create, :destroy]
+  get 'my_friends' => 'users#my_friends'
   
   
   # The priority is based upon order of creation: first created -> highest priority.
